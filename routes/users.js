517b7757feb420f7452a355912b9ea71caef4c0c
Michael Scharff <minka.scharff@gmail.com>
2022-09-14 19:03:46 +0300

    User.username ==> required: false;

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,6 @@
 const router = require('express').Router();
-const { getCurrentUser, getUsers } = require('../controllers/users');
+const { getCurrentUser } = require('../controllers/users');
 
 router.get('/users/me', getCurrentUser);
-router.get('/users', getUsers);
 
 module.exports = router;
