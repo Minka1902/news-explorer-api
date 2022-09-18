ba2edea4a2daabc11e1b1c1c23061138e72de0db
Michael Scharff <minka.scharff@gmail.com>
2022-09-18 18:22:57 +0300

    adding AUTHOR property to models/article.js

--- models/article.js
+++ models/article.js
@@ -6,6 +6,10 @@ const articleSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  author:{
+    type: String,
+    required: false,
+  },
   title: {
     type: String,
     required: true,
