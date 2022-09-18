e44a340d1a2d7fbdc4cd8bc2e796c01dfcadc633
Michael Scharff <minka.scharff@gmail.com>
2022-09-18 18:36:36 +0300

    adding author: JOI (VALIDATION) property to routes/article.js

--- routes/articles.js
+++ routes/articles.js
@@ -9,13 +9,14 @@ router.get('/articles', getArticles);
 router.post('/articles', celebrate({
   body: Joi.object().keys({
     keyword: Joi.string().required().min(2).max(30),
+    author: Joi.string().max(60),
     title: Joi.string().required().min(2),
     text: Joi.string().required().min(2),
     date: Joi.date().required(),
     source: Joi.string().required(),
     link: Joi.string().required().uri(),
     image: Joi.string().required().uri(),
-    owner: Joi.string().required(),
+    owner: Joi.string().required().min(23).max(25),
   }),
 }), createArticle);
 
