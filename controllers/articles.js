eaba21bbb3d1cd96776f36c8c297929df5fc7387
Michael Scharff <minka.scharff@gmail.com>
2022-10-02 19:31:15 +0300

    changing findByIdAndRemove ==> findByIdAndDelete

--- controllers/articles.js
+++ controllers/articles.js
@@ -7,7 +7,7 @@ const { handleError } = require('../errors/ErrorHandler');
 // ! request structure
 // ? req.body = {articleId: "article ID"}
 module.exports.deleteArticle = (req, res) => {
-  Article.findByIdAndRemove(req.params.id)
+  Article.findByIdAndDelete(req.params.id)
     .orFail()
     .then((article) => {
       if (req.user._id.toString() === article.owner) {
