c1f2d06431323932503131a80980b52a79f05dc4
Michael Scharff <minka.scharff@gmail.com>
2022-10-09 13:07:08 +0300

    09/10 13:07

--- controllers/users.js
+++ controllers/users.js
@@ -10,29 +10,46 @@ module.exports.createUser = (req, res) => {
   console.log('Create user Function');
   const { email, password, username } = req.body;
 
-  User.create({ email, password, username })
-  .then((user) => {
-    res.send({data: user});
-  })
-  .catch((err) => {
-    if (err.name === 'ValidationError') {
-      res.status(400).send(err);
-    } else {
-      console.log(err.name);
-      res.status(500).send(err);
-    }
-  });
-  bcrypt.hash(req.body.password, 10)
+  // User.create({ email, password, username })
+  // .then((user) => {
+  //   res.send({data: user});
+  // })
+  // .catch((err) => {
+  //   if (err.name === 'ValidationError') {
+  //     res.status(400).send(err);
+  //   } else {
+  //     console.log(err.name);
+  //     res.status(500).send(err);
+  //   }
+  // });
+  bcrypt.hash(password, 10)
     .then((hash) => {
       console.log(`hash: ${hash}`);
       User.create({
-        username: req.body.username,
-        email: req.body.email,
+        username: username,
+        email: email,
         password: hash // adding the hash to the database,
-      });
+      })
+        .then((user) => {
+          res.send({ data: user });
+        })
+        .catch((err) => {
+          if (err.name === 'ValidationError') {
+            res.status(400).send(err);
+          } else {
+            console.log(err.name);
+            res.status(500).send(err);
+          }
+        });
     })
     .then((user) => res.send(user))
-    .catch((err) => res.status(400).send(err));
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(400).send(err);
+      } else {
+        res.status(500).send(err);
+      }
+    });
 };
 
 // ////////////////////////////////////////////////////////////////
@@ -43,13 +60,13 @@ module.exports.login = (req, res, next) => {
   console.log('Login Function');
   const { email, password } = req.body;
 
-  User.findOne({ email }).select('+password')
+  User.findOne({ email }).select('password')
     .then((user) => {
       if (!user) {
         throw new NotFoundError('No user with matching ID found');
       } else {
         console.log(`user password = ${user.password}`);
-        res.send({_id: user._id, email: user.email, username: user.username, savedArticles: user.savedArticles});
+        // res.send({ _id: user._id, email: user.email, username: user.username, savedArticles: user.savedArticles });
         return bcrypt.compare(password, user.password);
       }
     })
@@ -70,8 +87,11 @@ module.exports.login = (req, res, next) => {
 // GET /users/me
 module.exports.getCurrentUser = (req, res) => {
   console.log('Get current user Function');
-  User.find()
+  const { _id: userId } = req.user;
+  User.findById(userId)
     .orFail()
-    .then((users) => res.send({ data: users }))
+    .then((user) => {
+      res.send({ email: user.email, name: user.name });
+    })
     .catch((err) => res.status(500).send({ message: err }));
 };
