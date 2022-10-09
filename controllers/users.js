628c869e6e099ebb123c3d6f57d0fa5f375b286e
Michael Scharff <minka.scharff@gmail.com>
2022-10-09 16:55:42 +0300

    09/10 16:48

--- controllers/users.js
+++ controllers/users.js
@@ -85,6 +85,7 @@ module.exports.login = (req, res, next) => {
 // GET /users/me
 module.exports.getCurrentUser = (req, res) => {
   console.log('Get current user Function');
+  console.log(`req: ${req}`);
   const userId = req.user._id;
   console.log(`user id: ${userId}`);
   User.findById(req.user._id)
